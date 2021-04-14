3fa855dcb01f51ae27e609df4980b452b7c89098
haritskoding <haritscoding@gmail.com>
2021-04-14 21:33:13 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,53 @@
+import express from "express";
+import mongoose from "mongoose";
+import Cors from "cors";
+import Videos from "./models/tiktok.js"
+
+//app config
+const app = express();
+const port = process.env.PORT || 9000;
+const url = 'mongodb+srv://aris:aris@cluster0.oa9la.mongodb.net/tiktokDB?retryWrites=true&w=majority'
+
+//middleware
+app.use(express.json())
+app.use(Cors())
+
+//DB Config
+// mongoose.connect(url, {
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useFindAndModify: true,
+//     useUnifiedTopology: true
+// }).then(() => console.log('connected'))
+//     .catch((err) => console.log(err))
+
+//api endpoints
+app.get('/', (req, res) => {
+    res.status(200).send('tiktok runn')
+});
+
+app.post('/v2/posts', (req, res) => {
+    const dbVideos = req.body;
+    Videos.create(dbVideos, (err, data) => {
+        if (err)
+            res.status(500).send(err);
+        else
+            res.status(201).send(data);
+    })
+})
+
+app.get('/v2/posts', (req, res) => {
+    Videos.find((err, data) => {
+        if (err)
+            res.status(500).send(err);
+        else
+            res.status(200).send(data);
+    })
+})
+
+
+// app.listen(port,
+mongoose.connect(url,
+    { useUnifiedTopology: true, useNewUrlParser: true })
+    .then(() => app.listen(port, () => console.log(`server running ${port}`)))
+    .catch(err => console.log(err))
